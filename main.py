ba2f016cd363e74d1eb636035f0d776db27645e1
Jreddy0077 <127132793+Jreddy0077@users.noreply.github.com>
2024-11-08 22:56:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -388,7 +388,7 @@ elif selected == "Prediction Analytics":
             plt.ylabel("Laundering Count")
             st.pyplot(plt)
         with c22:
-            st.dataframe(top_to_banks_df,height=600)
+            st.dataframe(top_to_banks_df,height=650)
     
         with c11:
             filtered_df = df[df["is laundering"] == 1]
@@ -418,7 +418,7 @@ elif selected == "Prediction Analytics":
            plt.ylabel("Count")
            st.pyplot(plt)
         with c22:
-            st.dataframe(payment_format_counts_df)
+            st.dataframe(payment_format_counts_df,height=600)
         with c11:
             currency_counts = df[df["is laundering"] == 1]["payment currency"].value_counts()
             currency_counts_df = currency_counts.reset_index()
@@ -433,7 +433,7 @@ elif selected == "Prediction Analytics":
             
             st.pyplot(plt)
         with c22:
-            st.dataframe(currency_counts_df)
+            st.dataframe(currency_counts_df,height=600)
         with c11:
 
             receiving_currency_counts = df[df["is laundering"] == 1]["receiving currency"].value_counts()
